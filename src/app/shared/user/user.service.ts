bf1fb7f374f7ecd67660bd4c87ee5104af44b878
Xin Zhang <Xin.Zhang@ausmaq.com.au>
2016-12-08 17:20:31 +1100

    add the username

--- src/app/shared/user/user.service.ts
+++ src/app/shared/user/user.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable, Subject} from "rxjs/Rx";
 
 import {JsonApiService} from "../api/json-api.service";
+import {Headers, Response, Http} from '@angular/http';
 
 @Injectable()
 export class UserService {
@@ -12,16 +13,30 @@ export class UserService {
     username: 'Guest'
   };
 
-  constructor(private jsonApiService:JsonApiService) {
+  constructor(private jsonApiService:JsonApiService, private http: Http) {
     this.user = new Subject();
   }
 
   getLoginInfo():Observable<any> {
-    return this.jsonApiService.fetch('/user/login-info.json')
-      .do((user)=>{
-        this.userInfo = user;
-      this.user.next(user)
+    // return this.jsonApiService.fetch('/user/login-info.json')
+    //   .do((user)=>{
+    //     this.userInfo = user;
+    //   this.user.next(user)
+    // })
+    
+    return this.http.get("/api/user/me").map(res => {
+      return {
+        "username": res.json(),
+        "picture": "assets/img/avatars/male.png",
+        "activity": 0
+      }
     })
+      .do((user) => {
+        this.userInfo = user;
+        this.user.next(user)
+      });
+
   }
+
 
 }
